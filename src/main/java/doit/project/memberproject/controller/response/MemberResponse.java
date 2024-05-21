bbfacb957f05718156559d080edc779385598041
salmonco <xkqjsslsek80@naver.com>
2024-05-21 20:44:27 +0900

    Entity를 그대로 반환하면 생기는 일

--- src/main/java/doit/project/memberproject/controller/response/MemberResponse.java
+++ src/main/java/doit/project/memberproject/controller/response/MemberResponse.java
@@ -4,7 +4,7 @@ package doit.project.memberproject.controller.response;
 import lombok.Getter;
 
 @Getter
-public class MemberResponse {
+public class MemberResponse { // View 용 객체. DTO? (Entity를 그대로 반환하면 보안상의 문제, 순환 참조 에러 문제가 발생함. 따라서 이를 방지)
     Long memberId;
     String name;
     Integer age;
